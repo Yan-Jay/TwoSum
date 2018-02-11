4653c1911ac007f015c35c6017c69a5a6fa422ee
Yan-Jay <kj929409@gmail.com>
2018-02-11 12:25:17 +0800

    題目出自Leetcode中Two Sum 1.完成程式class 2.單元測試失敗

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {       
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var test = new TwoSumClass();
+            int[] input = { 2, 7, 11, 15 };
+            var target = 9;
+            int[] expect = {0,1 };
+            int[] actual = test.Convert(input, target);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
